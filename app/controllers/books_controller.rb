92ebd2fc2b6637c8f32d241e1c89be39fb46d9cf
haraguchi12 <masakazu09201201@gmail.com>
2021-03-29 06:16:18 +0000

    投稿フォームを作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
+  end
+  
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
