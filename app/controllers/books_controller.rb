56033cee3f7866caf37e2d2e755e117f0f1ae322
haraguchi12 <masakazu09201201@gmail.com>
2021-03-30 13:16:34 +0000

    投稿一覧を作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,11 +15,17 @@ class BooksController < ApplicationController
   
 
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def destroy
+  end
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
