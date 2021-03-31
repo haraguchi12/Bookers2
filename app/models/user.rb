46f2159826a64f8d0be23ee40d226c14590987c4
haraguchi12 <masakazu09201201@gmail.com>
2021-03-31 04:42:24 +0000

    コントローラーの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
+  
+  validates :name, uniqueness: true
+  validates :name, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
 end
