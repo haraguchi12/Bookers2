59a0ef7f7c10028406eb56a39d7476e025c6975c
haraguchi12 <masakazu09201201@gmail.com>
2021-04-06 08:38:41 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,12 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
+    @book = Book.find(params[:id])
     
   end
   
   def update
-    
+    @book = Book.find(params[:id])
     @user = User.find(params[:id])
    if @user.update(user_params)
      flash[:notice] = "successfully" 
