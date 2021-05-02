dc105af825537f0cbdb36ece94b74e79760076a2
haraguchi12 <masakazu09201201@gmail.com>
2021-05-02 08:19:38 +0000

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
  root to: 'homes#top'
  get  "/home/about" =>"homes#about"
- resources :books, only: [:new, :create, :index, :show, :destroy, :edit,:update]
+ resources :books, only: [:new, :create, :index, :show, :destroy, :edit,:update] do
+   resources :book_comments, only: [:create, :destroy]
+ end
  resources :users, only: [:show, :edit, :update, :index]
 end
