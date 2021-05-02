e29db70bd4d02c4cc59169e947325518e45939d1
haraguchi12 <masakazu09201201@gmail.com>
2021-05-02 08:07:29 +0000

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 
   validates :name, uniqueness: true
   validates :name, presence: true, length: {minimum: 2, maximum: 20 }
