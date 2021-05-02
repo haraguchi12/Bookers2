667ce69f1b1b7f852709f0f1902888882becc5e0
haraguchi12 <masakazu09201201@gmail.com>
2021-05-02 09:59:14 +0000

    いいね機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, uniqueness: true
   validates :name, presence: true, length: {minimum: 2, maximum: 20 }
