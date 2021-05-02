0eb66c3a9cb0adaf5dc9b6824ef3d944d0e779d9
haraguchi12 <masakazu09201201@gmail.com>
2021-05-02 10:37:55 +0000

    修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,6 @@ class FavoritesController < ApplicationController
    @book = Book.find(params[:book_id])
    favorite = @book.favorites.find_by(user_id: current_user.id)
    favorite.destroy
-   
+   redirect_to request.referer
   end
 end
