352c81f6888a6181d4972e1cd450ecbe3f7523f9
haraguchi12 <masakazu09201201@gmail.com>
2021-05-03 08:14:01 +0000

    リレーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+
   validates :name, uniqueness: true
   validates :name, presence: true, length: {minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
