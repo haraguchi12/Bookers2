a8da3162c766ed41b880b697c1c5e2f0d04679e5
haraguchi12 <masakazu09201201@gmail.com>
2021-05-03 09:09:12 +0000

    解説確認し追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,22 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+
+  # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
 
   validates :name, uniqueness: true
   validates :name, presence: true, length: {minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
-  
+
   def follow(user_id)
    relationships.create(followed_id: user_id)
   end
@@ -28,5 +35,5 @@ class User < ApplicationRecord
   def following?(user)
    followings.include?(user)
   end
-  
+
 end
