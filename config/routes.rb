1e0d04c9944e5f720182fe39b08c7e177f406735
haraguchi12 <masakazu09201201@gmail.com>
2021-05-05 08:09:18 +0000

    ルーティング追記

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
  resource :relationships, only: [:create, :destroy]
  get 'followings' => 'relationships#followings', as: 'followings'
  get 'followers' => 'relationships#followers', as: 'followers'
+ get '/search', to: 'search#search'
 end
 end
