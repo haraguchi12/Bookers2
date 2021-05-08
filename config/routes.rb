f16ace100b47499b3f764b506ebd412f9d8fc015
haraguchi12 <masakazu09201201@gmail.com>
2021-05-08 06:34:26 +0000

    記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
  root to: 'homes#top'
  get  "/home/about" =>"homes#about"
+ get '/search', to: 'search#search'
  resources :books, only: [:new, :create, :index, :show, :destroy, :edit,:update] do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
@@ -12,6 +13,6 @@ Rails.application.routes.draw do
  resource :relationships, only: [:create, :destroy]
  get 'followings' => 'relationships#followings', as: 'followings'
  get 'followers' => 'relationships#followers', as: 'followers'
- get '/search', to: 'search#search'
+ 
 end
 end
