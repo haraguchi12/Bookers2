702eeeff6766b2f6a7e2f58b57feb419dd7d22a2
haraguchi12 <masakazu09201201@gmail.com>
2021-05-10 04:35:07 +0000

    住所情報を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,14 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
 
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+    :email,
+    :name,
+    :postcode,
+    :prefecture_name,
+    :address_city,
+    :address_street,
+    :address_building
+  ])
   end
 end
